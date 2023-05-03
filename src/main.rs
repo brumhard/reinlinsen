f7ec6bb64d64e5bf48ef94d90f29751031442a36
Tobias Brumhard <code@brumhard.com>
2023-05-03 08:47:21 +0200

    Extract layers into fs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use anyhow::{anyhow, Result};
 use bollard::image::ListImagesOptions;
 use bollard::Docker;
 use futures::TryStreamExt;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fmt::{Debug, Display};
 use std::fs::{self, File};
 use std::path::Path;
 use std::sync::Arc;
@@ -10,36 +12,87 @@ use tar::Archive;
 use tokio::fs::OpenOptions;
 use tokio::io::AsyncWriteExt;
 use tokio::sync::Mutex;
+use tracing::instrument;
+
+const IMAGE: &str = "ghcr.io/brumhard/friday:0.1.0";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let image = "ghcr.io/brumhard/friday:0.1.0".to_string();
+    tracing_subscriber::fmt().init();
 
-    let base_name = name_from_image(&image);
+    let base_name = name_from_image(IMAGE);
     let tar_file = format!("{base_name}.tar");
 
-    save_image(&image, &tar_file).await?;
+    save_image(IMAGE, &tar_file).await?;
     extract_tar(&tar_file, &base_name)?;
+    let manifest = read_manifest(&format!("{base_name}/manifest.json"))?;
+    extract_layers(&manifest.layers, &base_name, &format!("{base_name}-fs"))?;
 
     Ok(())
 }
 
-fn extract_tar<P: AsRef<Path>>(path: P, out_dir: P) -> Result<()> {
+#[instrument(skip(layers))]
+fn extract_layers<P: AsRef<Path> + Debug + Display>(
+    layers: &[String],
+    tar_dir: P,
+    out_dir: P,
+) -> Result<()> {
+    _ = fs::remove_dir_all(&out_dir);
+    fs::create_dir(&out_dir)?;
+
+    for layer in layers {
+        tracing::info!(layer, "unpacking layer");
+        let file = File::open(format!("{tar_dir}/{layer}"))?;
+        let mut archive = Archive::new(file);
+        for file in archive.entries()? {
+            let mut f = file?;
+            f.unpack_in(&out_dir)?;
+        }
+    }
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Manifest {
+    #[serde(rename = "Config")]
+    config: String,
+    #[serde(rename = "RepoTags")]
+    repo_tags: Vec<String>,
+    #[serde(rename = "Layers")]
+    layers: Vec<String>,
+}
+
+fn read_manifest(path: &str) -> Result<Manifest> {
+    let manifest_str = fs::read_to_string(path)?;
+    let mut manifests: Vec<Manifest> = serde_json::from_str(&manifest_str)?;
+
+    if manifests.len() != 1 {
+        return Err(anyhow!("unexpected number of manifests in target dir"));
+    }
+
+    Ok(manifests.pop().unwrap())
+}
+
+#[instrument]
+fn extract_tar<P: AsRef<Path> + Debug>(path: P, out_dir: P) -> Result<()> {
+    tracing::info!("recreating output dir");
     let file = File::open(path)?;
     _ = fs::remove_dir_all(&out_dir);
     fs::create_dir(&out_dir)?;
 
+    tracing::info!("unpacking archive");
     let mut archive = Archive::new(file);
     for file in archive.entries()? {
         let mut f = file?;
         f.unpack_in(&out_dir)?;
-        println!("{}", f.path()?.display());
     }
 
     Ok(())
 }
 
-async fn save_image<P: AsRef<Path>>(image: &str, path: P) -> Result<()> {
+#[instrument]
+async fn save_image<P: AsRef<Path> + Debug>(image: &str, path: P) -> Result<()> {
+    tracing::info!("connecting to docker daemon");
     let docker = Docker::connect_with_local_defaults()?;
 
     let file = OpenOptions::new()
@@ -50,6 +103,7 @@ async fn save_image<P: AsRef<Path>>(image: &str, path: P) -> Result<()> {
         .await?;
     let file = Arc::new(Mutex::new(file));
 
+    tracing::info!("checking image ref");
     let list = docker
         .list_images(Some(ListImagesOptions {
             filters: HashMap::from([("reference", vec![image])]),
@@ -61,6 +115,7 @@ async fn save_image<P: AsRef<Path>>(image: &str, path: P) -> Result<()> {
         return Err(anyhow!("ref should match exactly one image"));
     }
 
+    tracing::info!("exporting image to tar");
     docker
         .export_image(image)
         .try_for_each_concurrent(100, |data| {
